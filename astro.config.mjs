454e05a53931ec6ed2b0db308ac5a48dc6d8fad7
Anıl Kılıç <anlklc@gmail.com>
2026-01-01 11:59:42 +0300

    just astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [react()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    imageService: "cloudflare"
+  })
 });
